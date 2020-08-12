ab9dd42264456f889f73db097a5b0e29e75ab904
andersondumont <anderson_dumont@yahoo.com.br>
2020-08-12 17:59:47 -0300

    .

--- front_end/index.js
+++ front_end/index.js
@@ -31,3 +31,13 @@ function trataResposta(res){
         document.getElementById("msgERRO").innerHTML = "<h3>Usuário desconhecido<h3>";
     }
 }
+function logar(objeto){
+    // agora a idéia é armazenar o objeto do usuário recebido na resposta dentro do
+    // armazenamento interno do browser
+
+    // em um sistema real eu não armazeno o objeto, mas um TOKEN
+
+    var objSTR = JSON.stringify(objeto);           // converti o objeto pra uma string
+    localStorage.setItem("ScheduleUSER",objSTR);   // armazenei no local storage
+    window.location = "home.html";                 // vou pra página principal
+}
